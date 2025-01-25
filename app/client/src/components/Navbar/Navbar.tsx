ed099a2e139516b9fce8f62174f17518089558e5
Suryansh Srivastava <93089882+Suryansh2204@users.noreply.github.com>
2025-01-26 02:17:27 +0700

    Update Navbar.tsx

--- app/client/src/components/Navbar/Navbar.tsx
+++ app/client/src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ isDarkMode, toggleTheme }) => {
   return (
     <nav className={`navbar ${isDarkMode ? "dark-navbar" : "light-navbar"}`}>
-      <div className="site-title">✍️ Word Wizard</div>
+      <div className="site-title">✍️ Textpectations</div>
       <div style={{ marginRight: "3rem" }}>
         {/* <ThemeToggle isDarkMode={isDarkMode} toggleTheme={toggleTheme} /> */}
       </div>
